4a1edfd5fa543637c683c13164b72159c1f9508b
kanak <kanak>
2024-05-27 16:12:09 +0530

    minor updates

--- bert-ft/scripts/inference.py
+++ bert-ft/scripts/inference.py
@@ -3,13 +3,13 @@ import torch
 import time
 
 s = time.time()
-model = AutoModelForSequenceClassification.from_pretrained("/home/kanak/Documents/dev/rust_ml/bert-ft/scripts/pytorch_model").cuda()
+model = AutoModelForSequenceClassification.from_pretrained("bert-ft/scripts/pytorch_model").cuda()
 tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
 
 
 
 # Example input text
-with open("/home/kanak/Documents/dev/rust_ml/trf-ort/data/test.txt") as f:
+with open("trf-ort/data/test.txt") as f:
     texts = f.readlines()
 
 # for text in texts:
